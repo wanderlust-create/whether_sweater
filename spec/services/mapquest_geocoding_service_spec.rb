1df8230b035ad5b576a9f3239922e6e3ac492369
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-23 16:15:40 -0500

    test: MapquestGeocodingService

---
+++ spec/services/mapquest_geocoding_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe MapquestGeocodingService, :vcr do
+  context 'class methods' do
+    context '#get_lat_lon' do
+      it 'returns the latitude and longitude for a requested city' do
+
+        search = MapquestGeocodingService.get_lat_lon('atlanta')
+        expect(search).to be_a Hash
+        expect(search[:results]).to be_an Array
+        expect(search[:results][0][:locations]).to be_an Array
+        expect(search[:results][0][:locations][0][:displayLatLng]).to be_a Hash
+        expect(search[:results][0][:locations][0][:displayLatLng][:lat]).to_not be_nil
+        expect(search[:results][0][:locations][0][:displayLatLng][:lng]).to_not be_nil
+      end
+    end
+  end
+end
