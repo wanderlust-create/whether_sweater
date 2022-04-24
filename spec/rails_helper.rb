d5d5c17b7db47fbc58d61620329a9e59b960c1cb
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-24 16:32:49 -0500

    feat: forecast_facade

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,9 +75,9 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<tldr_key>') { ENV['tldr_key'] }
-  config.filter_sensitive_data('<mediastack_key>') { ENV['mediastack_key'] }
-  config.default_cassette_options = { re_record_interval: 30.days }
-  # config.default_cassette_options = { :record => :new_episodes }
+  config.filter_sensitive_data('<open_key>') { ENV['open_key'] }
+  config.filter_sensitive_data('<map_key>') { ENV['map_key'] }
+  # config.default_cassette_options = { re_record_interval: 1.hour }
+  config.default_cassette_options = { :record => :new_episodes }
   config.configure_rspec_metadata!
 end
