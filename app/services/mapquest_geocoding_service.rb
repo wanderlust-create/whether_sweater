0501e1857da2bd08860f2011fd12833657d8671f
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 10:26:35 -0500

    commit 1

--- app/services/mapquest_geocoding_service.rb
+++ app/services/mapquest_geocoding_service.rb
@@ -3,6 +3,10 @@ class MapquestGeocodingService
     get_url("/geocoding/v1/address?key=#{ENV['map_key']}&location=#{location}")
   end
 
+  def self.get_directions(from, to)
+    get_url("/directions/v2/route?key=#{ENV['map_key']}&from=#{from}&to=#{to}")
+  end
+
   def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
