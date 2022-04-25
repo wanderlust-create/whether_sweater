6d62151ad0b0e25d94ccac86f3c5120a0f99530f
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 18:04:20 -0500

    Update munchies_request_spec.rb

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -11,16 +11,20 @@ RSpec.describe "Wheather Sweater API", type: :request do
         expect(munchies[:data]).to be_a Hash
         expect(munchies[:data][:id]).to be_nil
         expect(munchies[:data][:type]).to eq("munchie")
+
         expect(munchies[:data]).to have_key(:attributes)
         expect(munchies[:data][:attributes]).to have_key(:destination_city)
         expect(munchies[:data][:attributes][:destination_city]).to be_a String
+
         expect(munchies[:data][:attributes]).to have_key(:travel_time)
         expect(munchies[:data][:attributes][:travel_time]).to be_a String
+
         expect(munchies[:data][:attributes]).to have_key(:forecast)
         expect(munchies[:data][:attributes][:forecast]).to have_key(:summary)
         expect(munchies[:data][:attributes][:forecast][:summary]).to be_a String
         expect(munchies[:data][:attributes][:forecast]).to have_key(:temperature)
         expect(munchies[:data][:attributes][:forecast][:temperature]).to be_a String
+
         expect(munchies[:data][:attributes]).to have_key(:restaurant)
         expect(munchies[:data][:attributes][:restaurant]).to have_key(:name)
         expect(munchies[:data][:attributes][:restaurant][:name]).to be_a String
