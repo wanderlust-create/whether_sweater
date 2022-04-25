1d41be7ee7d062264aeb4ec1e0602f487b7dc883
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 11:00:41 -0500

    test: yelp service

--- spec/services/yelp_search_service_spec.rb
+++ spec/services/yelp_search_service_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe YelpService, :vcr do
   context 'class methods' do
     context '#get_restaurant_search' do
             it 'returns a specific type of restaurant' do
-              
+              categories = 'cafe'
+              location = 'atlanta'
+              search = YelpService.get_restaurant(categories, location)
+              expect(search).to be_a Hash
+              expect(search[:businesses]).to be_an Array
             end
           end
         end
