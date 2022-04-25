ec72486d1ef5ce207a67f953d406f76083260295
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 15:27:13 -0500

    fix address

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -2,8 +2,7 @@ class Restaurant
   attr_reader :name, :address
 
   def initialize(data)
-    require "pry"; binding.pry
     @name = data[:businesses][0][:name]
-    @address = data[:businesses][0][:location][:display_address].join
+    @address = data[:businesses][0][:location][:display_address].join(' ')
   end
 end
