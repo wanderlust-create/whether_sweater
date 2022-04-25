ee5fb0e797b853155a919792b898a8c30f57bd65
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 10:56:36 -0500

    commit 2

---
+++ spec/services/yelp_search_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe YelpService, :vcr do
+  context 'class methods' do
+    context '#get_restaurant_search' do
+            it 'returns a specific type of restaurant' do
+              
+            end
+          end
+        end
+      end
