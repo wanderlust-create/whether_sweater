1284210b0e642e74f9c68eb34a2e34f5f0cbae9f
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-26 18:09:37 -0500

    refactor

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -9,33 +9,31 @@ module Api
         destination = params[:destination]
         unit = 'imperial'
         time = DirectionsFacade.directions(origin, destination)
-          if time.class == String
-            render json: time, status: 200
-          else
-        eta = time.eta_time
-        destination_location = ForecastFacade.location(destination)
-        lat = destination_location.latitude
-        lon = destination_location.longitude
-        weather = ForecastFacade.road_trip_hourly_weather(lat, lon, unit)
-        eta_weather = weather[eta]
-        render json: RoadTripSerializer.api_format(origin, destination, time, eta_weather), status: 201
+        if time.class == String
+          render json: time, status: 200
+        else
+          eta = time.eta_time
+          destination_location = ForecastFacade.location(destination)
+          lat = destination_location.latitude
+          lon = destination_location.longitude
+          weather = ForecastFacade.road_trip_hourly_weather(lat, lon, unit)
+          eta_weather = weather[eta]
+          render json: RoadTripSerializer.api_format(origin, destination, time, eta_weather), status: 201
+        end
       end
-    end
 
       private
 
       def authenticate
         user = User.find_by(api_key: params[:api_key])
         if !user
-         render json: { error: 'Invalid API key' }, status: 401
+          render json: 'Invalid API key', status: 401
         end
       end
 
       def verify_params
-        if params[:api_key] == '' || params[:origin] == '' || params[:destination] == ''
-          render json: { error: 'Incomplete information' }, status: 401
-        elsif !params[:api_key] || !params[:origin] || !params[:destination]
-          render json: { error: 'You need api_key, origin, & destination params to make a sucessful request' }, status: 401
+        if !params[:api_key] || !params[:origin] || !params[:destination]
+          render json: 'You need api_key, origin, & destination params to make a sucessful request', status: 401
         end
       end
     end
--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -2,30 +2,28 @@
 
 require 'rails_helper'
 RSpec.describe 'Wheather Sweater API', type: :request do
+
   describe 'POST api/v1/road_trip' do
-    before :each do
-      User.destroy_all
-    end
     describe 'happy path' do
       it 'returns json request' do
-        user_data = {
+        User.destroy_all
+        user1_data = {
           "email": "whatever@example.com",
           "password": "password",
           "password_confirmation": "password"
         }
         headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-        post '/api/v1/users', headers: headers, params: JSON.generate(user_data)
+        post '/api/v1/users', headers: headers, params: JSON.generate(user1_data)
 
-        user = User.last
+        user1 = User.last
         trip_data = {
           "origin": "Denver,CO",
           "destination": "Pueblo,CO",
-          "api_key": user.api_key
+          "api_key": user1.api_key
         }
         headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
         post '/api/v1/road_trip', headers: headers, params: JSON.generate(trip_data)
         trip = JSON.parse(response.body, symbolize_names: true)
-
         expect(response).to be_successful
         expect(trip).to be_a Hash
         expect(trip).to have_key(:data)
@@ -45,6 +43,72 @@ RSpec.describe 'Wheather Sweater API', type: :request do
         expect(trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float).or be_a(Integer)
         expect(trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
         expect(trip[:data][:attributes][:weather_at_eta][:conditions]).to be_a String
+      end
+    end
+
+    describe 'sad path' do
+      it 'returns json error if incormation is missing' do
+        User.destroy_all
+        user2_data = {
+          "email": "lama@example.com",
+          "password": "happy1",
+          "password_confirmation": "happy1"
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/users', headers: headers, params: JSON.generate(user2_data)
+
+        user2 = User.last
+        trip_data = {
+          "origin": "Denver,CO",
+          "api_key": user2.api_key
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(trip_data)
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("You need api_key, origin, & destination params to make a sucessful request")
+      end
+      it 'returns json error if route is impossible' do
+        User.destroy_all
+        user3_data = {
+          "email": "frog@example.com",
+          "password": "happy1",
+          "password_confirmation": "happy1"
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/users', headers: headers, params: JSON.generate(user3_data)
+
+        user3 = User.last
+        trip_data = {
+          "origin": "Denver,CO",
+          "destination": "London",
+          "api_key": user3.api_key
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(trip_data)
+        expect(response.status).to eq(200)
+        expect(response.body).to eq("impossible route")
+      end
+
+      it 'returns json error if API key is invalid' do
+        User.destroy_all
+        user4_data = {
+          "email": "dog@example.com",
+          "password": "happy1",
+          "password_confirmation": "happy1"
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/users', headers: headers, params: JSON.generate(user4_data)
+
+        user4 = User.last
+        trip_data = {
+          "origin": "Denver,CO",
+          "destination": "London",
+          "api_key": "12345"
+        }
+        headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(trip_data)
+        expect(response.status).to eq(401)
+        expect(response.body).to eq("Invalid API key")
       end
     end
   end
