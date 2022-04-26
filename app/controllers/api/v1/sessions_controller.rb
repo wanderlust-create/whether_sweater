00c3c85749797dc378645719c6a08932a7ecdffb
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 21:42:02 -0500

    appease rubocop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,13 +5,13 @@ module Api
     class SessionsController < ApplicationController
       def create
         user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
+        if user&.authenticate(params[:password])
           session[:user_id] = user.id
           render json: UsersSerializer.api_format(user), status: 201
-        elsif !user.authenticate(params[:password]) 
-          render json: { error: "Your email and/or password are invalid" }, status: :bad_request
+        elsif !user.authenticate(params[:password])
+          render json: { error: 'Your email and/or password are invalid' }, status: :bad_request
         else
-          render json: { error: "LogIn Failed" }, status: :bad_request
+          render json: { error: 'LogIn Failed' }, status: :bad_request
         end
       end
     end
