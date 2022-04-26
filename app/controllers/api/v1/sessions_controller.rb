f5ec23ec1b7e687b07ff4117188e7c6255f39c93
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-25 21:36:26 -0500

    user log in

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: UsersSerializer.api_format(user), status: 201
+        elsif !user.authenticate(params[:password])
+          render json: { error: "Your email and/or password are invalid" }, status: :bad_request
+        else
+          render json: { error: "LogIn Failed" }, status: :bad_request
+        end
+      end
+    end
+  end
+end 
