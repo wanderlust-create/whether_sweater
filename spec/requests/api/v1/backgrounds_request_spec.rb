e1c0673b8081491e6dcbefd0c261f10063b485fd
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-26 12:16:30 -0500

    update index GET /api/v1/backgrounds service test to reflect new attributes

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -13,19 +13,28 @@ RSpec.describe 'Wheather Sweater API', type: :request do
 
         expect(background).to have_key(:data)
         expect(background[:data]).to be_a Hash
+
+        expect(background[:data]).to have_key(:type)
+        expect(background[:data][:type]).to eq('image')
+
         expect(background[:data]).to have_key(:id)
         expect(background[:data][:id]).to be_a String
 
-        expect(background[:data]).to have_key(:type)
-        expect(background[:data][:type]).to eq('background_photo')
-
         expect(background[:data]).to have_key(:attributes)
-        expect(background[:data][:attributes]).to have_key(:url)
-        expect(background[:data][:attributes][:url]).to be_a String
-        expect(background[:data][:attributes]).to have_key(:artist)
-        expect(background[:data][:attributes][:artist]).to be_a String
-        expect(background[:data][:attributes]).to have_key(:portfolio)
-        expect(background[:data][:attributes][:portfolio]).to be_a String
+        expect(background[:data][:attributes]).to have_key(:image)
+        expect(background[:data][:attributes][:image]).to be_a Hash
+        expect(background[:data][:attributes][:image]).to have_key(:location)
+        expect(background[:data][:attributes][:image][:location]).to be_a String
+        expect(background[:data][:attributes][:image]).to have_key(:image_url)
+        expect(background[:data][:attributes][:image][:image_url]).to be_a String
+        expect(background[:data][:attributes][:image]).to have_key(:credit)
+        expect(background[:data][:attributes][:image][:credit]).to have_key(:source)
+        expect(background[:data][:attributes][:image][:credit][:source]).to be_a String
+        expect(background[:data][:attributes][:image][:credit]).to have_key(:photographer)
+        expect(background[:data][:attributes][:image][:credit][:photographer]).to be_a String
+        expect(background[:data][:attributes][:image][:credit]).to have_key(:portfolio)
+        expect(background[:data][:attributes][:image][:credit][:portfolio]).to be_a String
+        expect(background[:data][:attributes][:image][:credit]).to have_key(:"Guidelines and Crediting")
       end
     end
   end
