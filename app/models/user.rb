463cbb3ab4b526358cb12528f3979df93c186437
Tamara Dowis <tamara.dowis@gmail.com>
2022-07-13 11:29:34 -0500

    appease rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :api_keys, as: :bearer
+  has_many :api_keys, as: :bearer, dependent: :destroy
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true
